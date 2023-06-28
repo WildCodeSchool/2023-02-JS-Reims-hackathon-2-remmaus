2d266a4ce63478746e55956974c2396e463e498e
Aylin Noiret <aylinndream@hotmail.fr>
2023-06-28 15:51:49 +0200

    Routes des catégories et des valeurs de référence

--- backend/src/router.js
+++ backend/src/router.js
@@ -10,4 +10,20 @@ router.put("/items/:id", itemControllers.edit);
 router.post("/items", itemControllers.add);
 router.delete("/items/:id", itemControllers.destroy);
 
+const categoryControllers = require("./controllers/categoryControllers");
+
+router.get("/category", categoryControllers.browse);
+router.get("/category/:id", categoryControllers.read);
+router.put("/category/:id", categoryControllers.edit);
+router.post("/category/:id", categoryControllers.add);
+router.delete("/items/:id", categoryControllers.destroy);
+
+const valeurrefControllers = require("./controllers/valeur_refControllers");
+
+router.get("/valeur_ref", valeurrefControllers.browse);
+router.get("/valeur_ref/:id", valeurrefControllers.read);
+router.put("/valeur_ref/:id", valeurrefControllers.edit);
+router.post("/valeur_ref/:id", valeurrefControllers.add);
+router.delete("/valeur_ref/:id", valeurrefControllers.destroy);
+
 module.exports = router;
