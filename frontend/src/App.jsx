09350306e1152a0963dd75660dc20c577aa5fdb9
Carenjot <carenjot.aurelien@hotmail.fr>
2023-06-28 15:09:32 +0200

    add routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
-import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <p>coucou</p>
+      <Routes>
+        <Route path="/" element="" />
+        <Route path="/categorize" element="" />
+        <Route path="/info" element="" />
+        <Route path="/info/:id" element="" />
+      </Routes>
     </div>
   );
 }
