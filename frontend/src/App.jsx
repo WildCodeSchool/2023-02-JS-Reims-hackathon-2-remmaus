441e433bfe81b27bb8abf09d22bd7a294c80c91d
Carenjot <carenjot.aurelien@hotmail.fr>
2023-06-28 16:41:46 +0200

    add menu categorize

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 
 import "./App.css";
+import Categorize from "./pages/Categorize";
+import Informations from "./pages/Informations";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element="" />
-        <Route path="/categorize" element="" />
-        <Route path="/info" element="" />
+        <Route path="/categorize" element={<Categorize />} />
+        <Route path="/info" element={<Informations />} />
         <Route path="/info/:id" element="" />
       </Routes>
     </div>
